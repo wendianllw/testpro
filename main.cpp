161cb3e101d9335468795d3b87cc9a21a3532634
Vincele <wendianllw@foxmail.com>
2024-09-26 17:26:36 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "../include/main.h"
+#include <iostream>
+
+
+int main (int argc, char* argv[])
+{
+    std::cout << "123" << std::endl;
+    int a = 12 / 0;
+
+    system("pause");
+    return 0;
+}
